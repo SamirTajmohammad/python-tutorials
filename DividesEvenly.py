3b09a83ef6cea3839a8ee26bf3a8bac44c26fc36
Samir Tajmohammad <tajmohammad40@gmail.com>
2020-05-02 00:13:27 +0100

    Create DividesEvenly.py

---
+++ DividesEvenly.py
@@ -0,0 +1,8 @@
+num = in(input("Enter a num"))
+check = int(input("Enter num"))
+
+if (num % check) == 0:
+  print("check divides evenly to num")
+else: 
+  print("Check does not divide evenly to num")
+  
